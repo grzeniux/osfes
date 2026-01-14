f118cd7725dcb1b5fad1a2166be9dab8641edaee
Grzegorz Pustelnik <grzeniux28@gmail.com>
2026-01-14 19:27:19 +0100

    updatezad7

--- Zad_RTOS7.cpp
+++ Zad_RTOS7.cpp
@@ -29,3 +29,12 @@ int main(void) {
     xTaskCreate(Thread2, "thread2", 512, NULL, 5, NULL);
     vTaskStartScheduler();
 }
+Odp:
+T=500
+
+1. Thread2 startuje jako pierwszy i blokuje sie na vTaskDelay(100)
+2. Uruchamia się Thread1 i ustala T=0, zabiera mutexa i ma go aż do T=500
+3. T=100, Thread2 próbuje wziąć mutexa ale jest zajety juz przez Thread1
+4. T=500, Thread1 oddaje mutexa i następuje wywłaszczenie - start Thread2
+5. T=500, Thread2 rezerwuje mutexa az do T=800 i oddaje go poczym usuwa się
+6. Thread1 wraca i wypisuje T=500
