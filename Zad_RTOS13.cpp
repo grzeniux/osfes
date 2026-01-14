6e10e0a82d1d75ec3a942d498bc39d3017c9d326
Grzegorz Pustelnik <grzeniux28@gmail.com>
2026-01-14 22:10:51 +0100

    zad13

--- Zad_RTOS13.cpp
+++ Zad_RTOS13.cpp
@@ -24,3 +24,19 @@ int main(void) {
     xTaskCreate(Thread2, "thread2", 512, NULL, 3, NULL);
     vTaskStartScheduler();
 }
+Odp:
+T = 2000
+
+1. Thread1 zaczyna i ustala czas T=0, następnie czeka przechodzi w stan blocked aż do T=1000
+2. T=0 Startuje Thread2 i przechodzi w stan blocked aż do T=3000
+3. T=1000, próba wzięcia mutexa przez Thread1, nieudana ponieważ jest pusty mutex
+4. T=2000, mija czas oczekiwania Thread1
+5. Funkcja xSemaphoreTake konczy sie pdFALSE ale odblokowuje Thread1 bo koniec czasu oczekiwania
+    Thread1 robi printf T=2000
+    Thread1 usuwa się
+5. T=3000, Thread2 oddaje semafor - ale Thread1 juz nie istnieje
+6. Thread2 usuwa się
+
+
+
+
