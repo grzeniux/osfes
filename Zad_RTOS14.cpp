3614f1f4833d47262e4fdac3479b2d44505456fd
Grzegorz Pustelnik <grzeniux28@gmail.com>
2026-01-14 22:29:11 +0100

    zad14

--- Zad_RTOS14.cpp
+++ Zad_RTOS14.cpp
@@ -31,3 +31,15 @@ int main(void) {
     xTaskCreate(Thread2, "thread2", 512, NULL, 3, NULL);
     vTaskStartScheduler();
 }
+Odp:
+T = 1
+
+1. Tworzymy pusty SemaforBinarny
+2. Thready maja ten sam prioryter wiec beda pracowac na przemian
+3. Thread1 zaczyna i ustawia T=0 rusza z pętlą i próbuje wziąc mutex ale sie mu nie udaje bo jest pusty
+    więc robi vTaskDelay(1) i oddaje procesor
+4. Zaczyna Thread2 i przechodzi w blocked aż do T=2000
+5. T=1, Thread1 przechodzi całą pętle aż do T=1000
+    Thread1 printuje T=1000 i usuwa się
+6. T=2000, Thread2 oddaje semafor ale to juz bez znaczenia bo Thread1 sie usunął, Thread2 usuwa się
+
