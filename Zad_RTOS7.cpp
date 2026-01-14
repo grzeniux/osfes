4fff5b5aa4509715d146279d5a54978c793ead02
Grzegorz Pustelnik <grzeniux28@gmail.com>
2026-01-14 19:46:03 +0100

    zad7

--- Zad_RTOS7.cpp
+++ Zad_RTOS7.cpp
@@ -30,3 +30,11 @@ int main(void) {
     xTaskCreate(Thread2, "thread2", 512, NULL, 3, NULL); // Priorytet 3
     vTaskStartScheduler();
 }
+Odp:
+T = 700
+
+1. Thread1 ma wyższy priorytet wiec startuje i ustala czas T=0, jest w stanie blocked aż do T=300
+2. Thread2 startuje i jest w stanie blocked aż do T=100
+3. T=100, Thread2 rezerwuje mutexa i ma go aż do T=600
+4. T=300, Thread1 budzi się i próbuje wziąć mutexa ale jest zajęty więc czeka max 500 (czyli do T=800)
+5. T=600, Thread1 przejmuje mutexa od Thread2 i ma  go aż do T=700 poczym go oddaje i wypisuje T=700
