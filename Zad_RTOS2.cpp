f83604dece4ad561637f741ee9ee6710d5e80710
Grzegorz Pustelnik <grzeniux28@gmail.com>
2026-01-14 18:41:41 +0100

    zad2

--- Zad_RTOS2.cpp
+++ Zad_RTOS2.cpp
@@ -29,3 +29,12 @@ int main(void) {
     xTaskCreate(Thread2, "thread2", 512, NULL, 5, NULL);
     vTaskStartScheduler();
 }
+Odp:
+T=500
+
+1. Thread2 ma wyższy priorytet więc uruchomi się jako pierwszy i wpadnie w vTaskDelay(100)
+2. Wtedy Thread1 przejmuje zadanie i ustala czas T=0, zajmuje mutexa i ma go aż do T=500
+3. T=100, Thread2 sie budzi i próbuje wziac mutex ale jest zajety przez Thread1 wiec Thread2 pozostaje blocked
+4. T=500, Thread1 oddaje mutex i wypisuje T=500 i usuwa się
+5. T=500, Thread2 zabiera mutex i trzyma go aż do T=800, nastepnie oddaje i usuwa się
+
