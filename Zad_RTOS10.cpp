ed4ba2221df9f196c1fb64e2cc37bac6d5b63b52
Grzegorz Pustelnik <grzeniux28@gmail.com>
2026-01-14 20:00:04 +0100

    zad10

--- Zad_RTOS10.cpp
+++ Zad_RTOS10.cpp
@@ -29,3 +29,12 @@ int main(void) {
     xTaskCreate(Thread2, "thread2", 512, NULL, 3, NULL);
     vTaskStartScheduler();
 }
+Odp:
+T = 4000
+
+1. Mamy takie same priorytety, więc zadania wykonują sie równoległe
+2. Startujemy z Thread1 ustala czas T=0 i czekamy aż do T=1000
+3. T=0, Thread2 startuje bierze mutex i ma go aż do T=3000
+4. T=1000, próba wzięcia mutexa przez Thread1 - nieudana bo Thread2, czeka max do T=6000
+5. T=3000, Thread2 oddaje mutex i usuwa się, a Thread1 bierze go i czeka az do T=4000
+6. Thread1 oddaje mutex printuje T=4000 i usuwa się
